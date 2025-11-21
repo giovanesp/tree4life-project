674e391f26e54ef07926f086d7dbd7d31970a803
Giovane Spengler <giovanesp@gmail.com>
2025-11-21 14:50:09 -0400

    fix: Ajustes na visualização e filtragem de dados na API

--- homenagens_api/urls_api.py
+++ homenagens_api/urls_api.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomenagemListCreate, EspecieList 
+from .views import HomenagemListCreate, HomenagemListAPIView, EspecieList 
 
 urlpatterns = [
-    path('homenagens/', HomenagemListCreate.as_view(), name='homenagem-list-create'),
+    #path('homenagens/', HomenagemListCreate.as_view(), name='homenagem-list-create'),
+    path('homenagens/', HomenagemListAPIView.as_view(), name='homenagem-list-filtered'),
     path('especies/', EspecieList.as_view(), name='especie-list'),
 ]
--- homenagens_api/views.py
+++ homenagens_api/views.py
@@ -3,6 +3,7 @@ from rest_framework.pagination import PageNumberPagination
 from django.shortcuts import render
 from .models import Homenagem, Especie
 from .serializers import HomenagemSerializer, EspecieSerializer
+from django.db.models import Q
 
 def home_view(request):
     """Renderiza o arquivo index.html, que contém o mapa Leaflet."""
@@ -29,3 +30,41 @@ class HomenagemListCreate(generics.ListCreateAPIView):
 class EspecieList(generics.ListAPIView):
     queryset = Especie.objects.all().order_by('nome_popular')
     serializer_class = EspecieSerializer
+    
+    
+class HomenagemListAPIView(generics.ListAPIView):
+    serializer_class = HomenagemSerializer
+    pagination_class = StandardResultsPagination 
+    
+    def get_queryset(self):
+        queryset = Homenagem.objects.all()
+        params = self.request.query_params
+
+        # 1. Obter os parâmetros da URL
+        ne_lat = params.get('ne_lat') 
+        ne_lng = params.get('ne_lng') 
+        sw_lat = params.get('sw_lat') 
+        sw_lng = params.get('sw_lng') 
+
+        if ne_lat and ne_lng and sw_lat and sw_lng:
+            try:
+                ne_lat = float(ne_lat)
+                ne_lng = float(ne_lng)
+                sw_lat = float(sw_lat)
+                sw_lng = float(sw_lng)
+                
+                if sw_lng <= ne_lng:
+                    # Caso normal (não cruza 180°)
+                    queryset = queryset.filter(
+                        latitude__gte=sw_lat, 
+                        latitude__lte=ne_lat, 
+                        longitude__gte=sw_lng, 
+                        longitude__lte=ne_lng
+                    )
+                else:
+                    pass 
+                
+            except ValueError:
+                pass
+
+        return queryset
